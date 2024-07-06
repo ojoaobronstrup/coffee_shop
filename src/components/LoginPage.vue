5931d5a5854bafda8c29d566459f7d67a3670fe0
João Brönstrup <joaog.gob@gmail.com>
2024-07-06 18:19:53 -0300

    feat: first commit

---
+++ src/components/LoginPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-carousel show-arrows="false" hide-delimiter-background cycle height="100vh">
+    <v-carousel-item v-for="slider in sliders"
+      :src="slider"
+      :key="slider"
+      cover
+    >
+      <div>
+        <h1>AOS QUE POUCO DORMEM E MUITO SONHAM</h1>
+        <h3>Seu refúgio diário de sabor e aroma</h3>
+        <v-btn>LOGIN</v-btn>
+      </div>
+    </v-carousel-item>
+  </v-carousel>
+</template>
+
+<script>
+export default {
+  name: 'LoginPage',
+  data() {
+    return {
+      sliders: ["/assets/coffee_1.jpeg", "/assets/coffee_2.jpeg", "/assets/coffee_3.jpeg"],
+    }
+  }
+}
+</script>
