c1944efee47b57fed12c73eba4551455e73eaad3
João Brönstrup <joaog.gob@gmail.com>
2024-07-07 16:57:03 -0300

    refactor: replace the component ui library

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,26 +1,47 @@
 <template>
-  <v-carousel show-arrows="false" hide-delimiter-background cycle height="100vh">
-    <v-carousel-item v-for="slider in sliders"
-      :src="slider"
-      :key="slider"
-      cover
-    >
-      <div>
-        <h1>AOS QUE POUCO DORMEM E MUITO SONHAM</h1>
-        <h3>Seu refúgio diário de sabor e aroma</h3>
-        <v-btn>LOGIN</v-btn>
-      </div>
-    </v-carousel-item>
-  </v-carousel>
+  <el-carousel height="100vh">
+    <el-carousel-item v-for="srcImage in srcImages" :key="srcImage">
+      <img class="carousel-item" :src="srcImage" alt="Wallpaper of people drinking coffee">
+    </el-carousel-item>
+    <el-container class="text-overlay">
+      <el-text class="text-main">AOS QUE POUCO DORMEM E MUITO SONHAM</el-text>
+      <el-text class="text-main">Seu refúgio diário de sabor e aroma</el-text>
+    </el-container>
+  </el-carousel>
 </template>
 
 <script>
-export default {
-  name: 'LoginPage',
-  data() {
-    return {
-      sliders: ["/assets/coffee_1.jpeg", "/assets/coffee_2.jpeg", "/assets/coffee_3.jpeg"],
+  export default {
+    name: 'LoginPage',
+    data() {
+      return {
+        srcImages: ["/assets/coffee_1.png", "/assets/coffee_2.png", "/assets/coffee_3.png"],
+      }
     }
   }
-}
 </script>
+
+<style>
+  .carousel-item {
+    width: 100vw;
+    height: 100vh;
+  }
+
+  .text-overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 2;
+    pointer-events: none;
+  }
+
+  .text-main {
+    color: white;
+  }
+</style>
+
--- src/main.js
+++ src/main.js
@@ -1,14 +1,9 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+const app = createApp(App)
 
-const vuetify = createVuetify({
-    components,
-    directives,
-})
-
-createApp(App).use(vuetify).mount('#app')
+app.use(ElementPlus)
+app.mount('#app')
